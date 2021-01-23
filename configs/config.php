e6f3cc00ef0ce9159a025748fba07862a1f852d5
Robin Appelman <robin@icewind.nl>
2021-01-23 16:39:51 +0100

    allow local connect

--- configs/config.php
+++ configs/config.php
@@ -3,5 +3,6 @@
     'memcache.local' => '\\OC\\Memcache\\APCu',
     'memcache.distributed' => '\\OC\\Memcache\\APCu',
     'memcache.locking' => '\\OC\\Memcache\\APCu',
+    'allow_local_remote_servers' => true,
 //PLACEHOLDER
 ];
